99c5005fccb8fb3c0679f1c42d9d9e5ea352156a
Manuel Barkhau <mbarkhau@gmail.com>
2019-05-23 01:14:24 +0200

    bump version to v201905.0006-beta

--- src/markdown_katex/__init__.py
+++ src/markdown_katex/__init__.py
@@ -10,7 +10,7 @@ uses KaTeX to generate html from tex.
 """
 
 
-__version__ = "v201905.0005-beta"
+__version__ = "v201905.0006-beta"
 
 from markdown_katex.extension import KatexExtension
 from markdown_katex.wrapper import tex2html, get_bin_path
