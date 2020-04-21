f3d73ea482cab3f2ded105eb1db60c3eb2f6d09b
Manuel Barkhau <mb@admetrics.io>
2020-04-21 07:42:51 +0000

    bump version to v202004.0014-beta

--- src/markdown_katex/__init__.py
+++ src/markdown_katex/__init__.py
@@ -10,7 +10,7 @@ uses KaTeX to generate html from tex.
 """
 
 
-__version__ = "v202001.0013-beta"
+__version__ = "v202004.0014-beta"
 
 from markdown_katex.wrapper import tex2html
 from markdown_katex.wrapper import get_bin_path
