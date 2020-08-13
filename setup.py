c42545f3148e72c057ffd799fcaf41627868ead8
Manuel Barkhau <mbarkhau@gmail.com>
2020-08-13 22:53:25 +0000

    bump version to v202008.1023

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setuptools.setup(
     author="Manuel Barkhau",
     author_email="mbarkhau@gmail.com",
     url="https://gitlab.com/mbarkhau/markdown-katex",
-    version="202008.1022",
+    version="202008.1023",
     keywords="markdown katex extension",
     description="katex extension for Python Markdown",
     long_description=long_description,
