9746d623139dbdfb266f1b6aa8588b880e39d7f3
Manuel Barkhau <mbarkhau@gmail.com>
2021-03-12 01:14:29 +0000

    update katex binaries

--- scripts/update_katex_bin.sh
+++ scripts/update_katex_bin.sh
@@ -3,12 +3,11 @@ set -e;
 
 PROJECT_DIR=$PWD
 cd ../KaTeX
-npm install commander
-npm install pkg
 
-git checkout package.json
+git checkout .
 git checkout master
 git pull
+
 TAG=$(git tag -l --sort=taggerdate | grep -E "^v[0-9]+\.[0-9]+.[0-9]+$" | tail -n 1)
 echo $TAG
 git checkout $TAG
@@ -17,7 +16,14 @@ rm -f cli-linux
 rm -f cli-macos
 rm -f cli-win.exe
 
-npx pkg --target node12-win-x64,node12-linux-x64,node12-macos-x64 cli.js
+npm install -g pkg
+npm install -g yarn
+npm install commander
+
+yarn
+yarn build
+
+pkg --target node10-win-x64,node10-linux-x64,node10-macos-x64 cli.js
 
 BIN=$PROJECT_DIR/src/markdown_katex/bin
 mkdir -p $BIN
