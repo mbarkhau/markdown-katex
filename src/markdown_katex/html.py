e0d1642e440b44a97bd0e11c81e69eb178f63693
Manuel Barkhau <mbarkhau@gmail.com>
2021-12-14 21:21:24 +0000

    update katex href

--- src/markdown_katex/html.py
+++ src/markdown_katex/html.py
@@ -5,8 +5,8 @@
 # SPDX-License-Identifier: MIT
 _STYLESHEET_LINK = """
 <link rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css"
-  integrity="sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET"
+  href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css"
+  integrity="sha384-R4558gYOUz8mP9YWpZJjofhk+zx0AS11p36HnD2ZKj/6JR5z27gSSULCNHIRReVs"
   crossorigin="anonymous" />
 """
 
